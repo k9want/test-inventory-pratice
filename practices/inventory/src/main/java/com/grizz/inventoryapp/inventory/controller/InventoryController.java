c8abb578b40f5e68b5e339f8af64b5344dc3b04e
Sangkwon Lee <sangkwon26@gmail.com>
2024-05-19 01:16:03 +0900

    Test: InventoryControllerTest 작성 (2)
    InventoryController가 의존하는 InventoryService는 현재 컴포넌트(Bean)으로 등록되어 있지 않아서 에러가 발생하지만,
    테스트 코드에서는 @MockBean으로 빈화 시켜서 넣기 때문에 Bean을 찾지 못한다는 에러가 발생하지 않는다. 즉, 테스트에선 MockBean을 주입받기에 에러가 발생하지 않음

--- practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/controller/InventoryController.java
+++ practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/controller/InventoryController.java
@@ -1,5 +1,29 @@
 package com.grizz.inventoryapp.inventory.controller;
 
+import com.grizz.inventoryapp.inventory.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/v1/inventory")
 public class InventoryController {
 
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/{itemId}")
+    Object findByItemId(@PathVariable String itemId) {
+        if (inventoryService.findByItemId(itemId) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        System.out.println("itemId = " + itemId);
+        return itemId;
+    }
 }
