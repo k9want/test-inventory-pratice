190c1395278b381b81df9ab24391e455914cebf4
Sangkwon Lee <sangkwon26@gmail.com>
2024-05-19 00:58:45 +0900

    Test: InventoryControllerTest 작성 (1)

--- practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/controller/InventoryControllerTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/controller/InventoryControllerTest.java
@@ -1,35 +1,99 @@
 package com.grizz.inventoryapp.inventory.controller;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.grizz.inventoryapp.inventory.InventoryService;
+import com.grizz.inventoryapp.inventory.service.domain.Inventory;
 import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
 
 @WebMvcTest(InventoryController.class)
 public class InventoryControllerTest {
 
+    // mock + bean
+    @MockBean
+    private InventoryService inventoryService;
+
     @Autowired
     private MockMvc mockMvc;
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
 
     @DisplayName("재고 조회")
     @Nested
     class GetStock {
+
+        final String itemId = "1";
+        final Long stock = 100L;
         @DisplayName("자산(상품)이 존재하지 않을 경우, 404 status와 error를 반환한다.")
         @Test
-        void test1() {
-            assertNull(mockMvc);
-            throw new NotImplementedTestException();
+        void test1() throws Exception {
+            // given
+            given(inventoryService.findByItemId(itemId))
+                .willReturn(null);
+
+            // when
+            final MvcResult result = mockMvc.perform(get("/api/v1/inventory/{itemId}", itemId))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+            // then
+            final String content = result.getResponse().getContentAsString();
+
+            // $.error.code = 404
+            // $.error.local_message = "자산이 존재하지 않습니다."
+
+            final JsonNode responseBody = objectMapper.readTree(content);
+            final JsonNode errorField = responseBody.get("error");
+            assertNotNull(errorField);
+            assertEquals(404, errorField.get("code").asInt());
+            assertEquals("자산이 존재하지 않습니다.", errorField.get("local_message").asText());
         }
 
         @DisplayName("정상인 경우, 200 status와 결과를 반환한다.")
         @Test
-        void test1000() {
-            throw new NotImplementedTestException();
+        void test1000() throws Exception {
+
+            // given
+            final Inventory inventory = new Inventory(itemId, stock);
+            given(inventoryService.findByItemId(itemId))
+                .willReturn(inventory);
+
+            // when
+            final MvcResult result = mockMvc.perform(get("/api/v1/inventory/{itemId}", itemId))
+                .andExpect(status().isOk())
+                .andReturn();
+
+            // then
+            final String content = result.getResponse().getContentAsString();
+
+            // $.data.item_id = {itemId}
+            // $.data.stock = {stock}
+
+            final JsonNode responseBody = objectMapper.readTree(content);
+            final JsonNode dataField = responseBody.get("data");
+
+            assertNotNull(dataField);
+            assertEquals(itemId, dataField.get("item_id").asText());
+            assertEquals(stock, dataField.get("stock").asLong());
         }
     }
 
