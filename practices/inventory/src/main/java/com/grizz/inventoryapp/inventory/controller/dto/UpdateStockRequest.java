1c66ecfb4de843e75a74005e1278981624d39de8
Sangkwon Lee <sangkwon26@gmail.com>
2024-05-20 12:36:50 +0900

    Test: 재고 수정 테스트 및 구현 완료 [1]

---
+++ practices/inventory/src/main/java/com/grizz/inventoryapp/inventory/controller/dto/UpdateStockRequest.java
@@ -0,0 +1,9 @@
+package com.grizz.inventoryapp.inventory.controller.dto;
+
+import org.jetbrains.annotations.NotNull;
+
+public record UpdateStockRequest(
+    @NotNull Long stock
+) {
+
+}
--- practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/controller/InventoryControllerTest.java
+++ practices/inventory/src/test/java/com/grizz/inventoryapp/inventory/controller/InventoryControllerTest.java
@@ -5,6 +5,7 @@ import static com.grizz.inventoryapp.test.assertion.Assertions.assertMvcErrorEqu
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,8 +18,8 @@ import com.grizz.inventoryapp.inventory.controller.consts.ErrorCodes;
 import com.grizz.inventoryapp.inventory.service.domain.Inventory;
 import com.grizz.inventoryapp.inventory.service.exception.InsufficientStockException;
 import com.grizz.inventoryapp.inventory.service.exception.InvalidDecreaseQuantityException;
+import com.grizz.inventoryapp.inventory.service.exception.InvalidStockException;
 import com.grizz.inventoryapp.inventory.service.exception.ItemNotFoundException;
-import com.grizz.inventoryapp.test.exception.NotImplementedTestException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -34,16 +35,14 @@ import org.springframework.test.web.servlet.MvcResult;
 @WebMvcTest(InventoryController.class)
 public class InventoryControllerTest {
 
-    // mock + bean
-    @MockBean
-    private InventoryService inventoryService;
-
-    @Autowired
-    private MockMvc mockMvc;
-
     private final ObjectMapper objectMapper = new ObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    // mock + bean
+    @MockBean
+    private InventoryService inventoryService;
+    @Autowired
+    private MockMvc mockMvc;
 
     @DisplayName("재고 조회")
     @Nested
@@ -51,6 +50,7 @@ public class InventoryControllerTest {
 
         final String itemId = "1";
         final Long stock = 100L;
+
         @DisplayName("자산(상품)이 존재하지 않을 경우, 404 status와 error를 반환한다.")
         @Test
         void test1() throws Exception {
@@ -93,9 +93,11 @@ public class InventoryControllerTest {
     @DisplayName("재고 차감")
     @Nested
     class DecreaseQuantity {
+
         final String itemId = "1";
         final Long quantity = 10L;
         final Long stock = 90L;
+
         @DisplayName("자산(상품)이 존재하지 않을 경우, 404 status와 error를 반환한다.")
         @Test
         void test1() throws Exception {
@@ -184,22 +186,72 @@ public class InventoryControllerTest {
     @DisplayName("재고 수정")
     @Nested
     class UpdateStock {
+
+        final String itemId = "1";
+        final Long stock = 100L;
+
         @DisplayName("자산(상품)이 존재하지 않을 경우, 404 status와 error를 반환한다.")
         @Test
-        void test1() {
-            throw new NotImplementedTestException();
+        void test1() throws Exception {
+            // given
+            given(inventoryService.updateStock(itemId, stock))
+                .willThrow(ItemNotFoundException.class);
+
+            // when
+            final String requestBody = "{\"stock\": " + stock + "}";
+            final MvcResult result = mockMvc.perform(
+                    patch("/api/v1/inventory/{itemId}/stock", itemId)
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+            // then
+            assertMvcErrorEquals(result, ErrorCodes.ITEM_NOT_FOUND);
         }
 
         @DisplayName("수정하려는 재고가 유효하지 않은 경우, 404 status와 error를 반환한다.")
         @Test
-        void test2() {
-            throw new NotImplementedTestException();
+        void test2() throws Exception {
+            // given
+            given(inventoryService.updateStock(itemId, stock))
+                .willThrow(InvalidStockException.class);
+
+            // when
+            final String requestBody = "{\"stock\": " + stock + "}";
+            final MvcResult result = mockMvc.perform(
+                    patch("/api/v1/inventory/{itemId}/stock", itemId)
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+            // then
+            assertMvcErrorEquals(result, ErrorCodes.INVALID_STOCK);
         }
 
         @DisplayName("정상인 경우, 200 status와 결과를 반환한다.")
         @Test
-        void test1000() {
-            throw new NotImplementedTestException();
+        void test1000() throws Exception {
+            // given
+            final Inventory inventory = new Inventory(itemId, stock);
+            given(inventoryService.updateStock(itemId, stock))
+                .willReturn(inventory);
+
+            // when
+            final String requestBody = "{\"stock\": " + stock + "}";
+            final MvcResult result = mockMvc.perform(
+                    patch("/api/v1/inventory/{itemId}/stock", itemId)
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+            // then
+            assertMvcDataEquals(result, dataField -> {
+                assertEquals(itemId, dataField.get("item_id").asText());
+                assertEquals(stock, dataField.get("stock").asLong());
+            });
         }
     }
 }
